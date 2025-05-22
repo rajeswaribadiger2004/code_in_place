4ace4be187c25de7de67aacf0db2f28d8f1a8585
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-05-22 19:12:18 +0300

    Create JigSaw_Karel.py

---
+++ JigSaw_Karel.py
@@ -0,0 +1,32 @@
+from karel.stanfordkarel import *
+
+
+def main():
+
+    move()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+
+
+
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()

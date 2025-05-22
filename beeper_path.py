4b6435f550a7a9d5842bdd3a5cecb9a9cce4a51d
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-05-22 19:14:52 +0300

    Create beeper_path.py

---
+++ beeper_path.py
@@ -0,0 +1,27 @@
+from karel.stanfordkarel import *
+
+def main():
+    while beepers_present():
+        move()
+
+                             # Final move past the last beeper
+
+                                        # Now Karel is on the last beeper or one past it
+
+                         # Delete this line and write your code here! :)
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
+# Final move past the last beeper
+
+                                                                                # Now Karel is on the last beeper or one past it
+
+                                                                                                         # Delete this line and write your code here! :)
+
+
+                                                                                                         # There is no need to edit code beyond this point
+
+                                                                                                    

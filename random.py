6520b0fa8dad6e72f20f2fba2e5b9801796913ac
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-05-22 19:28:11 +0300

    Create random.py

---
+++ random.py
@@ -0,0 +1,18 @@
+
+import random
+NUM_RANDOM= 10    
+MIN_RANDOM=1
+MAX_RANDOM=100
+def main():
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+    print(random.randint(MIN_RANDOM,MAX_RANDOM))
+if __name__ == '__main__':
+    main()

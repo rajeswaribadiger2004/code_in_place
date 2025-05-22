698afcee50d62d11b9f938fed40f6e20d022a00b
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-05-22 19:13:59 +0300

    Create piles.py

---
+++ piles.py
@@ -0,0 +1,37 @@
+from karel.stanfordkarel import *
+
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+
+def main():
+    move()
+    for i in range(10):
+        pick_beeper()
+    move()
+    move()
+    for i in range(10):
+
+        pick_beeper()
+    move()
+    move()
+    for i in range(10):
+        pick_beeper()
+    move()
+
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+
+    
+    # your code here
+   
+   
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
